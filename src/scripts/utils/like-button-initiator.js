99ad5884583ca6ef8ea34da173aa74f64c501396
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-05-16 12:38:08 +0700

    add feature like movies with idb

---
+++ src/scripts/utils/like-button-initiator.js
@@ -0,0 +1,49 @@
+import FavoriteMovieIdb from '../data/favoritemovie-idb';
+import { createLikeButtonTemplate, createLikedButtonTemplate } from '../views/templates/template-creator';
+
+/* eslint-disable no-underscore-dangle */
+const LikeButtonInitiator = {
+  async Infinity({ likeButtonContainer, movie }) {
+    this._likeButtonContainer = likeButtonContainer;
+    this._movie = movie;
+
+    await this._renderButton();
+  },
+
+  async _renderButton() {
+    const { id } = this._movie;
+
+    if (await this._isMovieExist(id)) {
+      this._renderLiked();
+    } else {
+      this._renderLike();
+    }
+  },
+
+  async _isMovieExist(id) {
+    const movie = await FavoriteMovieIdb.getMovie(id);
+    return !!movie;
+  },
+
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteMovieIdb.putMovie(this._movie);
+      this._renderButton();
+    });
+  },
+
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = createLikedButtonTemplate();
+
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteMovieIdb.deleteMovie(this._movie.id);
+      this._renderButton();
+    });
+  },
+};
+
+export default LikeButtonInitiator;
--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,13 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import { createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
         <div id="movie" class="movie"></div>
+        <div id="likeButtonContainer"></div>
       `;
   },
 
@@ -13,12 +15,20 @@ const Detail = {
     // Fungsi ini akan dipanggil setelah render()
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
-
-    console.log(movie);
-
     // TODO: tampilkan movie di dalam DOM
     const moviesContainer = document.querySelector('#movie');
     moviesContainer.innerHTML = createMovieDetailTemplate(movie);
+
+    LikeButtonInitiator.Infinity({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
